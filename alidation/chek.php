bd73a7fc8b999284ac396419df8a22ba9759f302
Yana <boreisha123@gmail.com>
2023-10-26 17:55:38 +0300

    first commit

---
+++ alidation/chek.php
@@ -0,0 +1,32 @@
+<?php
+$login = filter_var( trim($_POST ['login']), FILTER_SANITIZE_STRING);
+$pass = filter_var( trim($_POST ['password']), FILTER_SANITIZE_STRING);
+$name = filter_var( trim($_POST ['name']), FILTER_SANITIZE_STRING);
+
+//проверка количества с-в
+if(mb_strlen($login)<5 || mb_strlen($login)> 90 ) {
+    echo "Недопустимая длина логина";
+    exit();
+}elseif(mb_strlen($name)<3 || mb_strlen($name)> 30 ) 
+{
+    echo "Недопустимая длина имени";
+    exit();
+}elseif(mb_strlen($pass)<3 || mb_strlen($pass)> 10 ) 
+{
+   echo "Недопустимая длина пароля";
+    exit();
+}
+
+//хеширование пароля
+$pass = md5($pass."hujhgr678");
+
+//подключение к бд
+$mysql = new mysqli ('127.0.0.1:3306', 'root', '', 'test1');
+
+//работа с таблицей бд
+$mysql->query("INSERT INTO `users` (`login`, `pass`, `name`) VALUES('$login', '$pass', '$name')");
+$mysql->close();
+
+//переадресация на страницу с формой
+header('Location: ../index.php');
+?>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;600;700&family=Rubik+Mono+One&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="cssRegistration/styleRegistration2.css">
+    </head>
+    <style>
+        .active {
+            background: #DDDDDD;
+            border: 20;
+            border-radius: 0 60px 0 0;
+            padding: 27px;
+        } 
+    </style>
+    <body>
+        <div class="background">
+            <div class="container">
+                <?php
+                if($_COOKIE['user'] == ''):
+                ?>
+                 <div class="form-box1">
+                        <div class="btn-box">
+                            <button type="button" class="toggle-btn" onclick="log()">Вход</button>
+                            <button type="button" class="toggle-btn active"onclick="reg()">Регистрация</button>
+                        </div>
+                                <form  id="log" action="alidation/entrance.php" class="group" method="post">
+                                        <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"> <br>
+                                        <input type="password" class="form-control" name="password" id="password" placeholder="Введите пароль"><br>
+                                        <button class="btn btn-success1" type="submit">войти</button>
+                                 </form>
+                    </div>  
+            </div>
+            <?php else: ?>
+
+                <p> Привет, <?= $_COOKIE['user'] ?>. Нажмите <a href="/exit.php">здесь</a>, чтобы выйти.</p>
+            <?php endif;?>
+        </div>
+            <script>
+            // var x=document.getElementsById("log")   
+            
+            function reg() {
+                window.location.href='registration.php';
+           }
+      
+            //     var y=document.getElementsById("reg")
+        //     var z=document.getElementsById("btn")  
+                
+        // function reg() {
+        //      x.style.left = "-1000px"
+        //      y.style.left = "50px"
+        //      z.style.left = "110px"
+        //    }
+            </script>
+
+    </body>
+    </html>
