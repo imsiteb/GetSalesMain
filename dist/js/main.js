e123803be96249a945ff2a99cff80aa3e1bc1276
imsiteb <dianakononovic4@gmail.com>
2023-10-26 18:12:15 +0300

    second commit

---
+++ dist/js/main.js
@@ -0,0 +1,29 @@
+const menu = document.querySelector('.burger__menu')
+const btn = document.querySelector('.menu__item-burger')
+const body = document.body;
+
+if (menu && btn) {
+  btn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+  })
+}
+
+$(document).ready(function () {
+  $('.sliders').slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    draggable: false,
+    centerMode: false
+  });
+
+  $('.shop__sliders').slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    draggable: false,
+    centerMode: true
+  });
+})
