f3cd0414d4ffd2d3bf071dada8eddb94e59335da
imsiteb <dianakononovic4@gmail.com>
2023-11-07 17:51:30 +0300

    almost final

--- js/shop.js
+++ js/shop.js
@@ -1,39 +1,5 @@
-$(document).ready(function () {
 
-  $('.shop__sliders').slick({
-    arrows: true,
-    infinite: true,
-    centerMode: true,
-    slidesToShow: 3,
-    // responsive: [
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       arrows: false,
-    //       centerMode: true,
-    //       centerPadding: '40px',
-    //       slidesToShow: 3
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       arrows: false,
-    //       centerMode: true,
-    //       centerPadding: '40px',
-    //       slidesToShow: 1
-    //     }
-    //   }
-    // ]
-  });
 
-  $('#liked-img').on({
-    'click': function () {
-      var src = ($(this).attr('src') === 'img/liked_br.png')
-        ? 'img/to_like.png'
-        : 'img/liked_br.png';
-      $(this).attr('src',src);
-    }
-  });
-
-})
+$('.items-block__item').click(function(){
+  $(this).toggleClass('liked');
+});
